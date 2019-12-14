8bcbb6c944844c6a6df78dd0967093b0fa36f4ba
Shahid Shah <shahid@shah.org>
2019-12-14 14:41:05 -0500

    Allow setting up shell for other users

--- setup-shell.sh
+++ setup-shell.sh
@@ -8,9 +8,20 @@ title() {
 # DCC_HOME is the devcontainer-common directory
 export DCC_HOME=${DCC_HOME:-/etc/devcontainer-common}
 
-title "Setup command line productivity tools for the current user"
+# We use zsh and antigen plus some default utilities so install those
 apt-get install -y zsh zsh-antigen tree
-cp $DCC_HOME/shell/.zshrc ~/.zshrc
-cp $DCC_HOME/shell/.antigenrc ~/.antigenrc
-cp $DCC_HOME/shell/.p10k.zsh ~/.p10k.zsh
-cp $DCC_HOME/shell/.z ~/.z
+
+# We expect this script to be passed in the usernames we're setting up
+for user in root "$@"
+do
+    userHome=`eval echo "~$user"`
+
+    title "Setup command line productivity tools for $user in $userHome"
+    cp $DCC_HOME/shell/.zshrc $userHome/.zshrc
+    cp $DCC_HOME/shell/.antigenrc $userHome/.antigenrc
+    cp $DCC_HOME/shell/.p10k.zsh $userHome/.p10k.zsh
+    cp $DCC_HOME/shell/.z $userHome/.z
+
+    title "Switch shell for $user in to /bin/zsh"
+    usermod --shell /bin/zsh $user
+done
